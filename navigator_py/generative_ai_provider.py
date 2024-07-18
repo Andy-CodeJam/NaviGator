d468c92d649ea79500747ae55f75205529c583b7
aaweaver-actuary <andrewayersweaver@gmail.com>
2024-07-18 09:19:58 -0400

    reworked the gen ai objects, made them more general

--- navigator_py/generative_ai_provider.py
+++ navigator_py/generative_ai_provider.py
@@ -1,3 +1,5 @@
+"""Define the interface expected of a generative AI provider and an implementation for Azure OpenAI."""
+
 import os
 from dataclasses import dataclass
 from typing import Protocol
@@ -9,12 +11,21 @@ load_dotenv(find_dotenv())
 
 
 @dataclass
-class AIProvider(Protocol):
+class GenerativeAIProvider(Protocol):
+    """Protocol interface for a generative AI provider. At minimum,
+    a provider must have a client property and a connect method to
+    establish a connection to the provider."""
+
+    @property
+    def client(self) -> AzureOpenAI: ...
+
     def connect(self): ...
 
 
 @dataclass
 class AzureOpenAIProvider:
+    """Provider for Azure OpenAI API."""
+
     _client: AzureOpenAI | None = None
 
     def connect(self):
@@ -32,3 +43,9 @@ class AzureOpenAIProvider:
             azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
             api_version=os.environ["AZURE_OPENAI_API_VERSION"],
         )
+
+    @property
+    def client(self):
+        if self._client is None:
+            self.connect()
+        return self._client
--- navigator_py/generative_ai_request.py
+++ navigator_py/generative_ai_request.py
@@ -1,25 +1,85 @@
 import os
 from dataclasses import dataclass
+from typing import Protocol
 
 import openai
 from dotenv import find_dotenv, load_dotenv
-from openai import AzureOpenAI
 
-from navigator.azure_oai_provider import AzureOpenAIProvider
+from navigator_py.generative_ai_provider import (
+    AzureOpenAIProvider,
+    GenerativeAIProvider,
+)
 
 load_dotenv(find_dotenv())
 
 
 @dataclass
-class OpenAIRequest:
-    """Represents a request to the Azure OpenAI API."""
+class GenerativeAIRequest(Protocol):
+    """Protocol interface for a generative AI request. At minimum,
+    a request must have a connect method to establish a connection
+    to the provider and a prompt method to generate a response.
 
-    _ai_provider: AzureOpenAIProvider | None = None
+    Additionally requires two properties: system_prompt and post_user_prompt.
+
+    system_prompt: str
+        The initial context given to the AI model to level set the conversation.
+    post_user_prompt: str
+        The context given to the AI model after the user's input to continue
+        the conversation. Allows for specific instructions to be given to the model
+        re: how to format the response.
+    """
+
+    @property
+    def system_prompt(self) -> str: ...
+
+    @property
+    def post_user_prompt(self) -> str: ...
+
+    def connect(self): ...
+
+    def prompt(self, prompt: str): ...
+
+
+@dataclass
+class OpenAIRequest:
+    """Represents a basic request to the Azure OpenAI API."""
+
+    _ai_provider: GenerativeAIProvider | None = None
     _max_tokens: int = int(os.getenv("OPENAI_MAX_TOKENS", 60))
     _temperature: float = float(os.getenv("OPENAI_TEMPERATURE", 0.1))
     _system_prompt: str = (
         "You are a helpful AI assistant that helps users with their questions."
     )
+    _post_user_prompt: str = "Please provide a response to the user's question in a markdown format similar in structure to a blog post."
+    _chat_history: list[dict] | None = None
+
+    def __post_init__(self):
+        if self._chat_history is None:
+            self._chat_history = []
+
+    @property
+    def ai_provider(self) -> GenerativeAIProvider:
+        if self._ai_provider is None:
+            self._ai_provider = AzureOpenAIProvider()
+        return self._ai_provider
+
+    @property
+    def max_tokens(self) -> int:
+        """Return the maximum number of tokens to generate."""
+        return self._max_tokens
+
+    @max_tokens.setter
+    def max_tokens(self, value: int):
+        self._max_tokens = value
+
+    @property
+    def temperature(self):
+        """Return the temperature to use for generating completions."""
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value: float):
+        self._temperature = value
 
     @property
     def system_prompt(self):
@@ -28,6 +88,18 @@ class OpenAIRequest:
     @system_prompt.setter
     def system_prompt(self, value: str):
         self._system_prompt = value
+
+    @property
+    def post_user_prompt(self):
+        return self._post_user_prompt
+
+    @post_user_prompt.setter
+    def post_user_prompt(self, value: str):
+        self._post_user_prompt = value
+
+    @property
+    def chat_history(self):
+        return self._chat_history
 
     def connect(self):
         self._ai_provider.client.connect()
@@ -40,6 +112,7 @@ class OpenAIRequest:
     ):
         if max_tokens is None:
             max_tokens = self._max_tokens
+
         if temperature is None:
             temperature = self._temperature
 
@@ -51,38 +124,48 @@ class OpenAIRequest:
                 messages=[
                     {"role": "system", "content": self.system_prompt},
                     {"role": "user", "content": prompt},
+                    {"role": "system", "content": self.post_user_prompt},
                 ],
             )
 
-            return response.choices[0].message.content
+            return response.choices[0].message
+
         except openai.AuthenticationError as e:
             # Handle Authentication error here, e.g. invalid API key
+
             print(f"OpenAI API returned an Authentication Error: {e}")
 
         except openai.APIConnectionError as e:
             # Handle connection error here
+
             print(f"Failed to connect to OpenAI API: {e}")
 
         except openai.BadRequestError as e:
             # Handle connection error here
+
             print(f"Invalid Request Error: {e}")
 
         except openai.RateLimitError as e:
             # Handle rate limit error
+
             print(f"OpenAI API request exceeded rate limit: {e}")
 
         except openai.InternalServerError as e:
             # Handle Service Unavailable error
+
             print(f"Service Unavailable: {e}")
 
         except openai.APITimeoutError as e:
             # Handle request timeout
+
             print(f"Request timed out: {e}")
 
         except openai.APIError as e:
             # Handle API error here, e.g. retry or log
+
             print(f"OpenAI API returned an API Error: {e}")
 
         except Exception as e:
             # Handles all other exceptions
+
             print(f"An exception has occured: {e}")
